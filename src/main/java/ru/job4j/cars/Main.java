ea9c2d603af5613d1e9a4efaa26d98d29516c80e
konstantin <kotmix@bk.ru>
2022-11-24 21:22:56 +0400

    1. Инициализация - проект "АвтоМаг" [#504895]

--- src/main/java/ru/job4j/cars/Main.java
+++ src/main/java/ru/job4j/cars/Main.java
@@ -19,40 +19,6 @@ import java.util.logging.Logger;
 public class Main {
     private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
 
-    private Properties loadDbProperties() {
-        Properties cfg = new Properties();
-        try (BufferedReader io = new BufferedReader(
-                new InputStreamReader(
-                        Main.class.getClassLoader()
-                                .getResourceAsStream("db.properties")
-                )
-        )) {
-            cfg.load(io);
-        } catch (Exception e) {
-            throw new IllegalStateException(e);
-        }
-        try {
-            Class.forName(cfg.getProperty("jdbc.driver"));
-        } catch (Exception e) {
-            throw new IllegalStateException(e);
-        }
-        return cfg;
-    }
-
-    @Bean
-    public BasicDataSource loadPool() {
-        Properties cfg = loadDbProperties();
-        BasicDataSource pool = new BasicDataSource();
-        pool.setDriverClassName(cfg.getProperty("jdbc.driver"));
-        pool.setUrl(cfg.getProperty("jdbc.url"));
-        pool.setUsername(cfg.getProperty("jdbc.username"));
-        pool.setPassword(cfg.getProperty("jdbc.password"));
-        pool.setMinIdle(5);
-        pool.setMaxIdle(10);
-        pool.setMaxOpenPreparedStatements(100);
-        return pool;
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
         LOGGER.info("Go to http://localhost:8080/cars");
